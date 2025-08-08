9c836fdad75654de7f6a8f187b091afa197d87ee
Charlie <charlie.wyatt0@icloud.com>
2025-08-08 19:29:43 +1000

    feat: l2 loss calculator

--- src/builders/loss_calculator_builder.py
+++ src/builders/loss_calculator_builder.py
@@ -1,9 +1,27 @@
 from src.builders.base_builder import EnumBuilder
 from enum import Enum
+import torch
 
 
 class L2LossCalculator:
-    pass
+    def __init__(self):
+        self.loss_fn = torch.nn.MSELoss(reduction="mean")
+
+    def __call__(self, target_embeddings, predicted_embeddings):
+        """
+        Args:
+            target_embeddings: Tensor of shape (batch_size, embedding_dim)
+            predicted_embeddings: list of tensors, each of shape (batch_size, embedding_dim)
+        Returns:
+            Scalar loss
+        """
+        if not predicted_embeddings:
+            raise ValueError("predicted_embeddings is empty")
+
+        stacked_preds = torch.stack(predicted_embeddings, dim=0)
+        avg_pred = stacked_preds.mean(dim=0)
+
+        return self.loss_fn(avg_pred, target_embeddings)
 
 
 class LossStrategy(Enum):
